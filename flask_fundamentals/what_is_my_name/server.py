2df591639ac0e604a1f512c4461912fc188d9228
Mendel <mendelbak@gmail.com>
2017-11-02 14:15:45 -0500

    Finished What's My name Flask assignment. Uploading to learning platform

---
+++ flask_fundamentals/what_is_my_name/server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, redirect, request
+app = Flask(__name__)
+
+
+@app.route('/')
+def basic_info():
+    return render_template('index.html') 
+
+@app.route('/process', methods=['POST'])
+def process():
+    print request.form
+    return redirect('/')
+
+app.run(debug=True)
