69e1a13519f50e0f539bb7fe313d58ca5c348a11
Mendel <mendelbak@gmail.com>
2017-11-12 00:13:09 -0600

    Finished Random Word Generator assg. Uploading to learning platform

--- django/first_project/apps/random_word/views.py
+++ django/first_project/apps/random_word/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.utils.crypto import get_random_string
 
 def index(request):
-    response = "Hello, I am your first request!"
-    return HttpResponse(response)
+    try:
+        request.session["counter"]
+    except:
+        request.session["counter"] = 0
+    return render(request, "random_word/index.html")
+
+def generate(request):
+    request.session["counter"] += 1
+    request.session["random_word"] = {
+        "word" : get_random_string(length=14)
+        }
+    return redirect("/")
+
+def reset(request):
+    request.session["counter"] = 0
+    request.session["random_word"] = ""
+    return redirect("/")
